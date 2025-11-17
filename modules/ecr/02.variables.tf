95fc7691d8dbb8e01427c1e4a02254713b5b42ae
kacper <kacper.dlugosz125@gmail.com>
2025-11-17 13:22:08 +0100

    feat: add variables for ECR configuration

---
+++ modules/ecr/02.variables.tf
@@ -0,0 +1,9 @@
+variable "project_name" {
+  description = "The name of the project"
+  type        = string
+}
+
+variable "owner" {
+  description = "The owner of the project"
+  type        = string
+}
