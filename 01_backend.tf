3e99c9054d5a64b21e0d3ac798a7d4a2dd0bb68b
kacper <kacper.dlugosz125@gmail.com>
2025-11-17 09:52:40 +0100

    feat: create project structure

---
+++ 01_backend.tf
@@ -0,0 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket   = "amzn-s3-bucket-tfstate"
+    key      = "terraform.tfstate"
+    region   = "eu-central-1"
+    dynamodb_table = "terraform-state-lock"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+  }
+}
---
+++ 04_variables.tf
@@ -0,0 +1,8 @@
+#variables.terraform 
+
+variable "aws_region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+  default     = "eu-central-1"
+}
+  
