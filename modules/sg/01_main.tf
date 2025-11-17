53312a7cc5df6bb817333e7baa4b06a3e506e28b
kacper <kacper.dlugosz125@gmail.com>
2025-11-17 14:56:03 +0100

    feat: add sg inbound for alb

---
+++ modules/sg/01_main.tf
@@ -0,0 +1,18 @@
+resource "aws_security_group" "example" {
+  name        = "${var.project_name}-sg"
+  vpc_id      = var.vpc_id 
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [var.default_cidr]
+  }
+
+#   egress {
+#     from_port       = 0
+#     to_port         = 0
+#     protocol        = "-1"
+#     prefix_list_ids = [aws_vpc_endpoint.my_endpoint.prefix_list_id]
+#   }
+}
