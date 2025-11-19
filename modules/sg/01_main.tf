eca8e293da71d8375d20e1fc57c0fd96daed6f2e
kacper <kacper.dlugosz125@gmail.com>
2025-11-19 10:05:57 +0100

    feat: add  secourity group for ecs

--- modules/sg/01_main.tf
+++ modules/sg/01_main.tf
@@ -1,6 +1,7 @@
+# Secutiry Group for ALB
 resource "aws_security_group" "alb_sg" {
-  name        = "${var.project_name}-sg"
-  vpc_id      = var.vpc_id 
+  name   = "${var.project_name}-alb_sg"
+  vpc_id = var.vpc_id
 
   ingress {
     from_port   = 80
@@ -9,16 +10,44 @@ resource "aws_security_group" "alb_sg" {
     cidr_blocks = [var.default_cidr]
   }
 
-#   egress {
-#     from_port       = 0
-#     to_port         = 0
-#     protocol        = "-1"
-#     prefix_list_ids = [aws_vpc_endpoint.my_endpoint.prefix_list_id]
-#   }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [var.default_cidr]
+  }
 
-    tags = {
-        Name    = "${var.project_name}-sg"
-        Project = var.project_name
-        Owner   = var.owner
-    }
+  tags = {
+    Name    = "${var.project_name}-alb_sg"
+    Project = var.project_name
+    Owner   = var.owner
+  }
 }
+
+# Security Group for ECS
+resource "aws_security_group" "ecs_sg" {
+  name   = "${var.project_name}-ecs_sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port       = 8080
+    to_port         = 8080
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "${var.project_name}-ecs_sg"
+    Project = var.project_name
+    Owner   = var.owner
+  }
+}
+
+
