d359e6fa4a9a8cf05a2940a6af1c1075e7b57ae1
kacper <kacper.dlugosz125@gmail.com>
2025-11-19 10:05:06 +0100

    feat: create IAM Role for ecs task execution

---
+++ modules/roles/01_main.tf
@@ -0,0 +1,29 @@
+# IAM Role for ECS Task Execution
+resource "aws_iam_role" "ecs_execution_role" {
+  name = "ecs_execution_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Name    = "${var.project_name}-ecs-execution-role"
+    Project = var.project_name
+    Owner   = var.owner
+  }
+}
+
+
+resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy" {
+  role       = aws_iam_role.ecs_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
